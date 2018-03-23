39798a0a59286315c4dae09111ea10b565c3e12c
Victor Hugo <vhfdoliveira@ufrn.edu.br>
2018-03-23 10:10:22 -0300

    k

--- 03_MPI_Histogram/opencv_hello_world.cpp
+++ 03_MPI_Histogram/opencv_hello_world.cpp
@@ -1,27 +1,24 @@
-#include <opencv2/opencv.hpp>
-#include <stdlib.h>
 #include <stdio.h>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 
-int main( int argc, char** argv )
+int main(int argc, char** argv )
 {
-	char* imageName = "02_Imagens/bird-780x520.jpg";
-	Mat image;
-	
-	image = imread( imageName, IMREAD_COLOR );
-	
-	if( argc != 2 || !image.data )
-	{
-	printf( " No image data \n " );
-	return -1;
-	}
-	
-	namedWindow( imageName, WINDOW_AUTOSIZE );
-	
-	imshow( imageName, image );
-	
-	waitKey(0);	
-	
-	return 0;
+  
+    Mat image;
+    image = imread( "/home/vhfdoliveira2/03_MPI_Histogram/02_Imagens/bird-780x520.jpg", 1 );
+    
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    
+    waitKey(0);
+    
+    return 0;
 }
